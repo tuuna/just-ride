865a88a2146c2224d79672bd2b0e92befbed835d
vampirebitter <lj550566181@gmail.com>
2017-03-19 20:56:10 +0800

    update

--- resources/views/article/single.blade.php
+++ resources/views/article/single.blade.php
@@ -100,36 +100,6 @@
                 </div>--}}
                 <!-- 右侧 -->
 
-                <div>
-                    <button class="btn btn-default pull-right" onclick="show_modal()"
-                    >发送私信</button>
-                    <div class="modal fade" id="modal-send-message" tabindex="-1" role="dialog">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times:</button>
-
-                                    <h4 class="modal-title">
-                                        发送私信
-                                    </h4>
-                                </div>
-                                <div class="modal-body">
-                                    <textarea name="body" class="form-control" ></textarea>
-                                    <div class="alert alert-success" >
-                                        <strong>私信发送成功</strong>
-                                    </div>
-                                </div>
-
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-primary" @click="store">
-                                    发送私信
-                                    </button>
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <div class="group-control">
                     <button class="btn btn-primary">发生私信</button>
                 </div>
@@ -149,9 +119,10 @@
     </div>
 </div>
     <script>
-        function show_modal()
-        {
-            $(#modal-send-message).show();
-        }
+
+            $("#message_button").click( function() {
+                $("#modal-send-message").modal('show');
+            });
+
     </script>
 @endsection
---
+++ resources/views/message/sendCertainMessage.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.main')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">收信人：</div>
+                    <div class="panel-body">
+                        <form action="/write/new" method="post">
+                            {!! csrf_field() !!}
+                            <div class="form-group {{ $errors->has('title') ? ' has-error' : '' }}">
+                                <label for="title">标题</label>
+                                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+                                <input type="hidden" name="author" value="{{ Auth::user()->name }}">
+                                <input type="text" name="title" id="title" class="form-control" placeholder="输入标题" value="{{old('title')}}">
+                                @if ($errors->has('title'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('title') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+
+                            <div class="form-group">
+                                <label for="category">所属分类</label>
+                                <select class="form-control" name="cate_id">
+                                    @foreach($allCategories as $category)
+                                        <option value="{{ $category->id }}">{{ $category->title }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+
+                            <div class="form-group">
+                                <label for="body">内容</label>
+                                <script id="container" name="content" type="text/plain">
+                                    {!! old('content') !!}
+
+                                </script>
+                                @if ($errors->has('content'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('content') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+
+                            <button class="btn btn-success pull-right" type="submit">提交文章</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@section('js')
+    <!-- 实例化编辑器 -->
+    <script type="text/javascript">
+        var ue = UE.getEditor('container', {
+            toolbars: [
+                ['bold', 'italic', 'underline', 'strikethrough', 'blockquote', 'insertunorderedlist', 'insertorderedlist', 'justifyleft','justifycenter', 'justifyright',  'link', 'insertimage', 'fullscreen']
+            ],
+            elementPathEnabled: false,
+            enableContextMenu: false,
+            autoClearEmptyNode:true,
+            wordCount:false,
+            imagePopup:false,
+            autotypeset:{ indent: true,imageBlockLine: 'center' }
+        });
+        ue.ready(function() {
+            ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.
+        });
+
+    </script>
+@endsection
+@endsection
